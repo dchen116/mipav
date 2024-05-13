c014917b5bd013a9ef5d5f6b56f7e692d9844876
chend <chend@janelia.hhmi.org>
2024-05-13 10:49:48 -0400

    An AccurateModeListener interface was made for responding to the accurateModeListener

---
+++ src/org/janelia/mipav/plugins/worm/untwisting/AccurateModeListener.java
@@ -0,0 +1,7 @@
+package org.janelia.mipav.plugins.worm.untwisting;
+
+
+public interface AccurateModeListener {
+	// Add this method for responding to the accurateModeListener
+    void accurateModeChanged(boolean isAccurateMode);
+}
